0266c049f875b704348793fdfad22bce1abdacd2
Jon Stacey <jon@jonsview.com>
2009-04-18 00:05:07 -0500

    Detect MacFuse installed via MacPorts out of the box.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,18 +4,32 @@
 # This uses mkmf
 require 'mkmf'
 
-# This allows --with-fuse-dir, --with-fuse-lib, 
-dir_config('fuse')
+# Prepare some default values for dir_config
+include_default = nil
+lib_default     = nil
+if (RUBY_PLATFORM.include? "darwin") && (!find_library('fuse', 'main'))
+  include_default = '/opt/local/include'
+  lib_default     = '/opt/local/lib'
+end
 
-# Make sure FUSE is found.
-unless have_library('fuse') 
+# This allows --with-fuse-dir, --with-fuse-lib, 
+dir_config('fuse', include_default, lib_default)
+
+# Add any fuse-[dir|lib] config options
+if (with_config('fuse-dir'))
+  $CPPFLAGS << ' -I' + with_config('fuse-dir')
+end
+if (with_config('fuse-lib'))
+  $CPPFLAGS << ' -I' + with_config('fuse-lib')
+end
+
+unless have_library('fuse')
   puts "No FUSE library found!"
   exit
 end
 
-# OS X boxes have statvfs.h instead of statfs.h
-have_header('sys/statvfs.h')
+have_header('sys/statvfs.h') # OS X boxes have statvfs.h instead of statfs.h
 have_header('sys/statfs.h')
 
-# Ensure we have the fuse lib.
+# Create the makefile
 create_makefile('fusefs_lib')
